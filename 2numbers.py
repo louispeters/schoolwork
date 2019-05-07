6e6a7b4ef71c533fa8acbbe0d27f1956f8f5e971
louispeters <46196893+louispeters@users.noreply.github.com>
2019-05-07 19:19:14 +0100

    Update 2numbers.py

--- 2numbers.py
+++ 2numbers.py
@@ -1,8 +1,8 @@
 first = int(input("enter the first number"))
 second = int(input("enter the second number"))
 if first == second:
-    print("the numbers are the same")
+    print("The numbers are the same")
 elif first>second:
-    print("the first number is more than the second number")
+    print("The first number is more than the second number")
 else:
-    print("the second number is more than the first number")
+    print("The second number is more than the first number")
